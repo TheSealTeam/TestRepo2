6267fa05cddd83c39f1218ec085a6b963c0b08ca
Simon Olofsson <zigge112@gmail.com>
2016-10-30 20:30:22 +0100

    added uncertainty to turkey identity

--- Bwuk/Bwuk/main.c
+++ Bwuk/Bwuk/main.c
@@ -4,5 +4,6 @@ int main()
 	printf("Bwuk I am a chicken!\n");
 	printf("Maybe I am a turkey?");
 	printf("I have decided I am a turkey!\n") //our new branch
+    printf("For now...\n");
 	return 0;
 }
