a30dd369a5bd288ef1d89f0bd184869b5650cba6
Simon Olofsson <zigge112@gmail.com>
2016-10-30 20:56:31 +0100

    fixed formating

--- Bwuk/Bwuk/main.c
+++ Bwuk/Bwuk/main.c
@@ -4,6 +4,6 @@ int main()
 	printf("Bwuk I am a chicken!\n");
 	printf("Maybe I am a turkey?");
 	printf("I have decided I am a turkey!\n") //our new branch
-    printf("For now...\n");
+	printf("For now...\n");
 	return 0;
 }
